3ba2354bd488a492febb44e9ad047a61ad01505b
FloraMoses <flora2548@gmail.com>
2015-12-07 20:52:05 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -20,5 +20,41 @@ struct node {
 };
 
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	typedef struct node NODE;
+	NODE *head, *first = 0, *cur = 0;
+	if (N == 0) {
+		head = (NODE *)malloc(sizeof(NODE));
+		head->num = 0;
+		head->next = NULL;
+		return head;
+	}
+	if (N < 0)
+		N = -N;
+	int remainder = 0;
+	while (N != 0)
+	{
+		head = (NODE *)malloc(sizeof(NODE));
+		remainder = N % 10;
+		head->num = remainder;
+		N = N / 10;
+		if (first != 0)	{
+			cur->next = head;
+			cur = head;
+		}
+		else {
+			first = head;
+			cur = head;
+		}
+	}
+	cur->next = NULL;
+	cur = first;
+	NODE *previous = NULL, *next;
+	while (cur != NULL) {
+		next = cur->next;
+		cur->next = previous;
+		previous = cur;
+		cur = next;
+	}
+	cur = previous;
+	return cur;
 }
